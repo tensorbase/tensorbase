ee9c532a18bdbea5f3f34ae311afe72cf20742d6
Jin Mingjian <jin.phd@gmail.com>
2021-05-05 19:18:11 +0800

    clean engine::errs
    Signed-off-by: Jin Mingjian <jin.phd@gmail.com>

--- crates/engine/src/errs.rs
+++ crates/engine/src/errs.rs
@@ -4,23 +4,8 @@ use thiserror::Error;
 pub type EngineResult<T> = std::result::Result<T, EngineError>;
 #[derive(Debug, Error)]
 pub enum EngineError {
-    #[error(transparent)]
-    WrappingMetaError(#[from] meta::errs::MetaError),
-
-    #[error(transparent)]
-    WrappingLangError(#[from] lang::errs::LangError),
-
-    #[error(transparent)]
-    WrappingIOError(#[from] std::io::Error),
-
-    #[error("Unexpected gen error which should not happen")]
-    UnexpectedGenError,
-
     #[error("Unexpected data loading error which should not happen")]
     UnexpectedDataLoadingError,
-
-    #[error("Invalid HIR gen result error")]
-    InvalidHIRGenResultError,
 
     #[error("Table does not exist")]
     TableNotExist,
@@ -31,26 +16,24 @@ pub enum EngineError {
     #[error("Column info does not exist")]
     ColumnInfoNotExist,
 
-    #[error("Unsupported limit value")]
-    UnsupportedLimitValueError,
-
     #[error("Unsupported Bql type")]
     UnsupportedBqlType,
 
-    #[error(
-        "Keys in group by clause can not matched with those in select clause"
-    )]
-    KeyInGroupByNotMatched,
-
     #[error("Error when unwrappring Option")]
     UnwrapOptionError,
-
-    #[error("Error when unwrappring Result")]
-    UnwrapResultError,
 
     #[error(transparent)]
     WrappingDFError(#[from] datafusion::error::DataFusionError),
 
     #[error(transparent)]
     WrappingArrowError(#[from] arrow::error::ArrowError),
+
+    #[error(transparent)]
+    WrappingMetaError(#[from] meta::errs::MetaError),
+
+    #[error(transparent)]
+    WrappingLangError(#[from] lang::errs::LangError),
+
+    #[error(transparent)]
+    WrappingIOError(#[from] std::io::Error),
 }
