29154f281b004b781c30b2d111e18b60decec478
Jin Mingjian <jin.phd@gmail.com>
2021-07-15 13:50:49 +0800

    FEAT(base): contract API abstraction
    Signed-off-by: Jin Mingjian <jin.phd@gmail.com>

--- crates/base/src/contract.rs
+++ crates/base/src/contract.rs
@@ -1,6 +1,32 @@
 #[macro_export]
 macro_rules! contract {
-    ($x:expr) => {
-        assert!($x)
+    ($($c:tt)*) => {
+        debug_assert!($($c)*);
     };
+}
+
+#[cfg(test)]
+mod unit_tests {
+    fn test_fn_01(x: i32, y: i64) -> i64 {
+        contract!(x < 0 && x > -100, "x should be negative integer and larger that -100");
+        contract!(y > 100, "y should postive integer");
+
+        let r = x as i64 + y;
+
+        contract!(r > 0);
+        r
+    }
+
+    #[test]
+    #[cfg(debug_assertions)]
+    #[should_panic]
+    fn basic_check_debug() {
+        test_fn_01(1, 99);
+    }
+
+    #[test]
+    #[cfg(not(debug_assertions))]
+    fn basic_check_release() {
+        test_fn_01(1, 99);
+    }
 }
