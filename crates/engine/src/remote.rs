4b9b5abddea6566460cea349d9f9bf85b3152ec4
Jin Mingjian <jin.phd@gmail.com>
2021-08-06 09:15:34 +0800

    FEAT(engine,runtime,server,tests_integ): support tls tcp protocol solve part of #227
    Signed-off-by: Jin Mingjian <jin.phd@gmail.com>

--- crates/engine/src/remote.rs
+++ crates/engine/src/remote.rs
@@ -1,26 +1,20 @@
 use crate::errs::EngineResult;
 use client::prelude::{Pool, ServerBlock};
-use std::lazy::SyncLazy;
-use tokio::runtime::{self, Runtime};
-
-static TOKIO_RT: SyncLazy<Runtime> = SyncLazy::new(|| {
-    runtime::Builder::new_current_thread()
-        .enable_io()
-        .enable_time()
-        .build()
-        .unwrap()
-});
+use tokio::runtime::Handle;
 
 pub fn run(pool: &Pool, sql: &str) -> EngineResult<Vec<ServerBlock>> {
-    TOKIO_RT.block_on(async move {
-        let mut conn = pool.connection().await?;
-        let mut query_result = conn.query(sql).await?;
-        let mut blocks = vec![];
+    let res: EngineResult<_> = tokio::task::block_in_place(|| {
+        Handle::current().block_on(async move {
+            let mut conn = pool.connection().await?;
+            let mut query_result = conn.query(sql).await?;
+            let mut blocks = vec![];
 
-        while let Some(block) = query_result.next().await? {
-            blocks.push(block);
-        }
+            while let Some(block) = query_result.next().await? {
+                blocks.push(block);
+            }
 
-        Ok(blocks)
-    })
+            Ok(blocks)
+        })
+    });
+    Ok(res?)
 }
--- crates/server/src/server.rs
+++ crates/server/src/server.rs
@@ -4,12 +4,15 @@ use std::{
     env,
     fs::File,
     io::{self, BufReader},
+    path::Path,
+    sync::Arc,
 };
 
 use baselog::{ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
 use bytes::BytesMut;
-use futures::future;
+use futures::{future, FutureExt};
 use log::info;
+use meta::confs::{Tcp, Tls};
 use runtime::{
     mgmt::{BMS, READ, REMOTE_READ, WRITE},
     read::{query, remote_query},
@@ -17,6 +20,9 @@ use runtime::{
 };
 use server::BaseSrvConn;
 use tokio::net::TcpListener;
+use tokio_rustls::rustls::internal::pemfile::{certs, rsa_private_keys};
+use tokio_rustls::rustls::{Certificate, NoClientAuth, PrivateKey, ServerConfig};
+use tokio_rustls::TlsAcceptor;
 
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
@@ -58,22 +64,69 @@ async fn main() -> io::Result<()> {
     REMOTE_READ.get_or_init(|| remote_query);
 
     // start servers
-    if let Some(tcp_srv) = &conf.server.tcp {
-        log::info!("server: {:?}", tcp_srv);
-        let srv_addr =
-            [tcp_srv.ip_addr.as_str(), tcp_srv.port.to_string().as_str()].join(":");
-        let listener = TcpListener::bind(srv_addr).await?;
-
-        loop {
-            let (io, _) = listener.accept().await?;
-            tokio::spawn(BaseSrvConn {
-                io,
-                read_buf: BytesMut::with_capacity(4096),
-                write_buf: BytesMut::with_capacity(4096),
-                conn_ctx: Default::default(),
-            });
-        }
+    let mut servers = vec![];
+    if let Some(tcp) = &conf.server.tcp {
+        log::info!("server: {:?}", tcp);
+        servers.push(fut_tcp_srv(tcp).boxed());
     }
+    if let Some(tls) = &conf.server.tls {
+        log::info!("server: {:?}", tls);
+        servers.push(fut_tls_srv(tls).boxed());
+    }
+    if servers.is_empty() {
+        panic!("can not find a valid server setup in conf!");
+    }
+    future::try_join_all(servers).await?;
 
     Ok(())
+}
+
+fn load_certs(path: &Path) -> io::Result<Vec<Certificate>> {
+    certs(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
+}
+
+fn load_keys(path: &Path) -> io::Result<Vec<PrivateKey>> {
+    rsa_private_keys(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid key"))
+}
+
+async fn fut_tcp_srv(tcp: &Tcp) -> io::Result<()> {
+    let srv_addr = [tcp.ip_addr.as_str(), tcp.port.to_string().as_str()].join(":");
+    let listener = TcpListener::bind(srv_addr).await?;
+
+    loop {
+        let (io, _) = listener.accept().await?;
+        tokio::spawn(BaseSrvConn {
+            io,
+            read_buf: BytesMut::with_capacity(4096),
+            write_buf: BytesMut::with_capacity(4096),
+            conn_ctx: Default::default(),
+        });
+    }
+}
+
+async fn fut_tls_srv(tls: &Tls) -> io::Result<()> {
+    let srv_addr = [tls.ip_addr.as_str(), tls.port.to_string().as_str()].join(":");
+    let path_cert = Path::new(&tls.certificate_file);
+    let path_pkey = Path::new(&tls.private_key_file);
+    let certs = load_certs(&path_cert)?;
+    let mut keys = load_keys(&path_pkey)?;
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    config
+        .set_single_cert(certs, keys.remove(0))
+        .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))?;
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+    let listener = TcpListener::bind(srv_addr).await?;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let acceptor = acceptor.clone();
+        let tls = acceptor.accept(stream).await?;
+        tokio::spawn(BaseSrvConn {
+            io: tls,
+            read_buf: BytesMut::with_capacity(4096),
+            write_buf: BytesMut::with_capacity(4096),
+            conn_ctx: Default::default(),
+        });
+    }
 }
